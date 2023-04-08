f565c1c0f7a0238fedb04afee6d2f0b79bd128fd
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-04-08 20:06:19 +0200

    Add project files.

---
+++ ScreenRecorder/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ScreenRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonStop = new Button();
+            checkBox1 = new CheckBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // buttonStop
+            // 
+            buttonStop.Location = new Point(56, 71);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(78, 29);
+            buttonStop.TabIndex = 1;
+            buttonStop.Text = "stop";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Click += buttonStop_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(56, 12);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(78, 24);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Record";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(217, 199);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
+            Controls.Add(buttonStop);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button buttonStop;
+        private CheckBox checkBox1;
+        private Label label1;
+    }
+}
